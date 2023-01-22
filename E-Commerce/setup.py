65b7207e6ae4181a10afc1390193563e3b99781d
devabdth <a.tharwat@cubersio.com>
2023-01-22 11:50:27 +0200

    Increase Max Content Length

--- E-Commerce/setup.py
+++ E-Commerce/setup.py
@@ -9,7 +9,7 @@ def setup(app: Flask):
 	app.config["SESSION_PERMANENT"] = False
 	app.config["SESSION_TYPE"] = "filesystem"
 	app.config["DEBUG"]= True
-	app.config['MAX_CONTENT_LENGTH'] = 5120
+	app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024
 
 	Session(app)
 	QRcode(app)
